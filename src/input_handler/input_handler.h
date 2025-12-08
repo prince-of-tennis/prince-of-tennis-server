80ffbb509e6bee30ee454b309c81fb178748f6fa
AoI40104 <aoi.ohata2434@gmail.com>
2025-12-08 16:36:14 +0900

    入力と打球のシステムを作ったよ

--- src/input_handler/input_handler.h
+++ src/input_handler/input_handler.h
@@ -2,5 +2,6 @@
 
 #include "common/player.h"
 #include "common/player_input.h"
+#include "common/ball.h"
 
-void apply_player_input(Player &player, const PlayerInput &input, float deltaTime);
+void apply_player_input(Player &player, Ball &ball, const PlayerInput &input, float deltaTime);
